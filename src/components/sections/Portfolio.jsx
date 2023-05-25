c7bf88b8ff50f592012a6c17b67827354cb03c34
Jerick Dela Rosa <delarosajerick26@gmail.com>
2023-05-25 22:27:29 +0800

    Mapping projects

--- src/components/sections/Portfolio.jsx
+++ src/components/sections/Portfolio.jsx
@@ -1,31 +1,38 @@
 import React from 'react'
 import { mechKeeb1 } from '../../assets'
+import { projects } from '../../constants'
 
-const ProjectTemplate = ({ title, description, projectImg }) => (
-    <div className="mx-2 xs:mx-8 ss:mx-14 sm:mx-auto lg:px-16 px-0 self-center">
-        <div className="mt-3 lg:-mx-6 lg:flex lg:items-center">
-            <img className="object-cover w-full lg:mx-6 lg:w-1/2 rounded-xl sm:h-72 lg:h-96" src={projectImg} alt="" />
+const ProjectTemplate = ({ title, description, projectImg, languages, developers, index }) => (
+    <div className="mt-3 lg:-mx-6 md:flex lg:items-center">
+        <img className={`order-1 ${index % 2 == 0 ? "md:order-2" : "md:order-1"} object-cover w-full md:mx-6 md:w-1/2 rounded-xl sm:h-72 md:h-96`} src={projectImg} alt="" />
 
-            <div className="mt-6 lg:w-1/2 lg:mt-0 lg:mx-6 ">
-                <p className="font-poppins block mt-4 text-xl font-semibold uppercase text-green md:text-2xl">
-                    {title}
-                </p>
+        <div className={`order-2 ${index % 2 == 0 ? "md:order-1" : "md:order-2"} mt-6 md:w-1/2 md:mt-0 md:mx-6`}>
+            <p className="font-poppins block text-xl font-semibold uppercase text-green md:text-2xl">
+                {title}
+            </p>
 
-                <p className="mt-3 text-sm text-gray-500 dark:text-gray-300 md:text-sm">
-                    {description}
-                </p>
+            <p className="font-poppins mt-3 text-sm text-dimWhite md:text-base">
+                {description}
+            </p>
 
-                <a href="#" className="inline-block mt-2 text-blue-500 underline hover:text-blue-400">Read more</a>
+            <div className="flex items-center mt-6">
+                {languages.map((lang, index) => (
+                    <p key={`${lang.id}-${index}`} className="inline-block mx-1 font-poppins tracking-wider rounded bg-dimGreen px-4 pb-2 pt-2 text-xs font-bold uppercase text-white">
+                        {lang.title}
+                    </p>
+                ))}
+            </div>
 
-                <div className="flex items-center mt-6">
-                    <img className="object-cover object-center w-10 h-10 rounded-full" src={mechKeeb1} alt="" />
+            {developers.map((dev, index) => (
+                <div key={`${dev.id}-${index}`} className="flex items-center mt-6">
+                    <img className="object-cover object-center w-10 h-10 rounded-full" src={projectImg} alt="" />
 
                     <div className="mx-4">
-                        <h1 className="text-sm text-gray-700 dark:text-gray-200">Amelia. Anderson</h1>
-                        <p className="text-sm text-gray-500 dark:text-gray-400">Lead Developer</p>
+                        <h1 className="text-sm text-gray-700 dark:text-gray-200">{dev.title}</h1>
+                        <p className="text-sm text-gray-500 dark:text-gray-400">{dev.designation}</p>
                     </div>
                 </div>
-            </div>
+            ))}
         </div>
     </div>
 )
@@ -43,45 +50,11 @@ const Portfolio = () => (
         </div>
 
         <div className="mx-2 xs:mx-8 ss:mx-14 sm:mx-auto lg:px-16 px-0 self-center">
-            <div className="mt-3 lg:-mx-6 lg:flex lg:items-center">
-                <img className="object-cover w-full lg:mx-6 lg:w-1/2 rounded-xl sm:h-72 lg:h-96" src={mechKeeb1} alt="" />
 
-                <div className="mt-6 lg:w-1/2 lg:mt-0 lg:mx-6">
-                    <p className="font-poppins block text-xl font-semibold uppercase text-green md:text-2xl">
-                        Document Tracking System
-                    </p>
+            {projects.map((project, index) => (
+                <ProjectTemplate key={project.id} {...project} index={index} />
+            ))}
 
-                    <p className="font-poppins mt-3 text-sm text-dimWhite md:text-base">
-                        A website application for Bulacan State University to help them track their internal organizational document transactions.
-                    </p>
-
-                    <div className="flex items-center mt-6">
-                        <p className="inline-block mx-1 font-poppins tracking-wider rounded bg-dimGreen px-4 pb-2 pt-2 text-xs font-bold uppercase text-white">
-                            Vue
-                        </p>
-                        <p className="inline-block mx-1 font-poppins tracking-wider rounded bg-dimGreen px-4 pb-2 pt-2 text-xs font-bold uppercase text-white">
-                            Laravel
-                        </p>
-                    </div>
-
-                    <div className="flex items-center mt-6">
-                        <img className="object-cover object-center w-10 h-10 rounded-full" src={mechKeeb1} alt="" />
-
-                        <div className="mx-4">
-                            <h1 className="text-sm text-gray-700 dark:text-gray-200">Kevin Kim De Vera</h1>
-                            <p className="text-sm text-gray-500 dark:text-gray-400">Lead Developer</p>
-                        </div>
-                    </div>
-                    <div className="flex items-center mt-6">
-                        <img className="object-cover object-center w-10 h-10 rounded-full" src={mechKeeb1} alt="" />
-
-                        <div className="mx-4">
-                            <h1 className="text-sm text-gray-700 dark:text-gray-200">Jerick Dela Rosa</h1>
-                            <p className="text-sm text-gray-500 dark:text-gray-400">Web Developer</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
         </div>
     </section>
 )
--- src/constants/index.js
+++ src/constants/index.js
@@ -12,7 +12,10 @@ import {
     coding,
     ideas,
     programming,
-    webDesign
+    webDesign,
+    mechKeeb1,
+    mechKeeb2,
+    mechKeeb3
 } from '../assets'
 
 
@@ -105,4 +108,76 @@ export const skills = [
         content: "Modern and contemporary codes are utilized in the project."
     }
 ];
+
+export const projects = [
+    {
+        id: "project-1",
+        title: "Document Tracking System",
+        description: "A website application for Bulacan State University to help them track their internal organizational document transactions.",
+        projectImg: mechKeeb1,
+        languages: [
+            {
+                id: "lang-1",
+                title: "Vue"
+            },
+            {
+                id: "lang-2",
+                title: "Laravel"
+            },
+            {
+                id: "lang-3",
+                title: "MySQL"
+            },
+        ],
+        developers: [
+            {
+                id: "dev-1",
+                title: "Kevin Kim De Vera",
+                designation: "Lead Web Developer"
+            },
+            {
+                id: "dev-2",
+                title: "Jerick Dela Rosa",
+                designation: "Web Developer"
+            },
+        ]
+    },
+    {
+        id: "project-2",
+        title: "Electronic Records Management System",
+        description: "A website application for Philippine Charity Sweepstakes Office to help them track their internal organizational document transactions.",
+        projectImg: mechKeeb2,
+        languages: [
+            {
+                id: "lang-1",
+                title: "Angular"
+            },
+            {
+                id: "lang-2",
+                title: ".NET C#"
+            },
+            {
+                id: "lang-3",
+                title: "MS SQL"
+            },
+        ],
+        developers: [
+            {
+                id: "dev-1",
+                title: "Joseph Reynaldo",
+                designation: "Lead Web Developer"
+            },
+            {
+                id: "dev-2",
+                title: "Jerimaiah Peralta",
+                designation: "Web Developer"
+            },
+            {
+                id: "dev-3",
+                title: "Jerick Dela Rosa",
+                designation: "Web Developer"
+            },
+        ]
+    },
+]
 
